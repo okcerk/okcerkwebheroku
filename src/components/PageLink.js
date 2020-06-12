c56c1d14bb6882a8cade986153343bb79889a390
okcerk <okcerk@gmail.com>
2020-06-12 15:23:57 -0700

    fixing blog button

--- src/components/PageLink.js
+++ src/components/PageLink.js
@@ -1,26 +1,39 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 const style = {
-    pageLink: {
-        cursor: 'pointer',
-        margin: 20,
-    },
-    linkImage: {
-        maxWidth: 600,
-    }
+  pageLink: {
+    cursor: "pointer",
+    marginRight: 20,
+    marginLeft: 20,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  linkImage: {
+    maxWidth: 600,
+  },
 };
 
 class PageLink extends Component {
-    render() {
-        const image = (<img style={style.linkImage} src={this.props.image} alt={this.props.title}/>);
-        return (            
-            <div style={style.pageLink}>
-                {this.props.link && (<Link to={this.props.link}>{image}</Link>)}
-                {this.props.externalUri && (<a href={this.props.externalUri} target='_blank'>{image}</a>)}
-            </div>
-        );
-    }
-};
+  render() {
+    const image = (
+      <img
+        style={style.linkImage}
+        src={this.props.image}
+        alt={this.props.title}
+      />
+    );
+    return (
+      <div style={style.pageLink}>
+        {this.props.link && <Link to={this.props.link}>{image}</Link>}
+        {this.props.externalUri && (
+          <a href={this.props.externalUri} target="_blank">
+            {image}
+          </a>
+        )}
+      </div>
+    );
+  }
+}
 
 export default PageLink;
