dc9921712c3ee9f198466d5b4b2b88d53b30c59e
okcerk <okcerk@gmail.com>
2020-06-13 18:28:31 -0700

    adding ink artworks

--- src/contentConfigs/inkConfig.js
+++ src/contentConfigs/inkConfig.js
@@ -1,26 +1,90 @@
-import inktober2018Link from "../images/link-inktober2018.png";
-import inktober2019Link from "../images/link-inktober2019.png";
-
 export const inkConfig = {
   key: "ink",
   items: [
     {
-      header: "Under construction!",
-      text:
-        "Come back soon! (or not). Meanwhile you can check out the Inktober pages, which are also inky pieces of art -->",
-      type: "TextFrame",
+      text: "End of the road",
+      image: "/assets/ink/end_of_road",
+      type: "SketchFrame",
     },
     {
-      title: "Inktober 2018",
-      image: inktober2018Link,
-      link: "/inktober2018",
-      type: "LinkFrame",
+      text: "Fish person",
+      image: "/assets/ink/fish_person",
+      type: "SketchFrame",
     },
     {
-      title: "Inktober 2019",
-      image: inktober2019Link,
-      link: "/inktober2019",
-      type: "LinkFrame",
+      text: "The visit",
+      image: "/assets/ink/the_visit",
+      type: "SketchFrame",
+    },
+    {
+      text: "Termotanque",
+      image: "/assets/ink/termotanque",
+      type: "SketchFrame",
+    },
+    {
+      text: "Monday mornings",
+      image: "/assets/ink/monday_mornings",
+      type: "SketchFrame",
+    },
+    {
+      text: "Madman",
+      image: "/assets/ink/madman",
+      type: "SketchFrame",
+    },
+    {
+      text: "Tax Season",
+      image: "/assets/ink/tax_season",
+      type: "SketchFrame",
+    },
+    {
+      text: "That place you never got to go",
+      image: "/assets/ink/desert",
+      type: "SketchFrame",
+    },
+    {
+      text: "Where will you be when the chocolate is all gone?",
+      image: "/assets/ink/chocolate_gone",
+      type: "SketchFrame",
+    },
+    {
+      text: "The Lost World",
+      image: "/assets/ink/the_lost_world",
+      type: "SketchFrame",
+    },
+    {
+      text: "Fire",
+      image: "/assets/ink/fire",
+      type: "SketchFrame",
+    },
+    {
+      text: "Last Goodbye",
+      image: "/assets/ink/last_goodbye",
+      type: "SketchFrame",
+    },
+    {
+      text: "Little boy",
+      image: "/assets/ink/little_boy",
+      type: "SketchFrame",
+    },
+    {
+      text: "Rat",
+      image: "/assets/ink/rat",
+      type: "SketchFrame",
+    },
+    {
+      text: "Vacation planning",
+      image: "/assets/ink/vacation_planning",
+      type: "SketchFrame",
+    },
+    {
+      text: "Warming up",
+      image: "/assets/ink/warming_up",
+      type: "SketchFrame",
+    },
+    {
+      text: "Wo ist mein Wochenende?",
+      image: "/assets/ink/wochenende",
+      type: "SketchFrame",
     },
   ],
 };
