f9d44a6d36f13d1bd9b26468c82d0ae2dafda160
okcerk <okcerk@gmail.com>
2020-09-13 01:47:23 -0700

    adding hd episodes

--- src/contentConfigs/homepageConfig.js
+++ src/contentConfigs/homepageConfig.js
@@ -394,7 +394,7 @@ export const homepageConfig = {
       configKey: "digital",
     },
     "e37-9": {
-      url: "https://www.youtube.com/embed/h63QPrgaMHM",
+      url: "https://www.youtube.com/embed/jJvd1qRYvec",
       description:
         "Squad 37: Episode 9. Cacho invites a bunch of superheroes to the spaceship for a meeting. Soda will try to kill everyone. SPANISH",
       type: "YoutubeFrame",
@@ -466,7 +466,7 @@ export const homepageConfig = {
       configKey: "digital",
     },
     "e37-8": {
-      url: "https://www.youtube.com/embed/9BwRET5O6BE",
+      url: "https://www.youtube.com/embed/HnV71gO53cU",
       description:
         "Squad 37: Episode 8. Octopus buys some new toys that become alive when no one is watching. They will also try to steal furniture. SPANISH",
       type: "YoutubeFrame",
@@ -533,7 +533,7 @@ export const homepageConfig = {
       configKey: "digital",
     },
     "e37-7": {
-      url: "https://www.youtube.com/embed/1tvWTspx08M",
+      url: "https://www.youtube.com/embed/bzffAAIkgCA",
       description:
         "Squad 37: Episode 7. Jigsaw wants to test the Squad with deathly puzzles in order to teach them a life lesson. Will they survive?. SPANISH",
       type: "YoutubeFrame",
@@ -600,7 +600,7 @@ export const homepageConfig = {
       configKey: "comics",
     },
     "e37-6": {
-      url: "https://www.youtube.com/embed/hhFY-TNanD4",
+      url: "https://www.youtube.com/embed/PfTXLWowHM4",
       description:
         "Squad 37: Episode 6. K-K is a famous Hollywood actor and wont stop making movies. SPANISH",
       type: "YoutubeFrame",
@@ -667,33 +667,33 @@ export const homepageConfig = {
       configKey: "digital",
     },
     "e37-5": {
-      url: "https://www.youtube.com/embed/Z8dAt71oyvQ",
+      url: "https://www.youtube.com/embed/gCAOdisqthU",
       description:
         "Squad 37: Episode 5. Pajaroman gets a visit from his ex girlfriend while Cloro creates a monster capable of eating everything, including people. SPANISH",
       type: "YoutubeFrame",
     },
     "e37-4": {
-      url: "https://www.youtube.com/embed/_higrY--Ag8",
+      url: "https://www.youtube.com/embed/i1twByefUA8",
       description:
-        "Squad 37: Episode 4. Richard purchases a car online and wants to drive it while Cloro is planning on leaving the spaceship forever. SPANISH WITH ENGLISH SUBTITLES",
+        "Squad 37: Episode 4. Richard purchases a car online and wants to drive it while Cloro is planning on leaving the spaceship forever. SPANISH",
       type: "YoutubeFrame",
     },
     "e37-3": {
-      url: "https://www.youtube.com/embed/-90SsdtvaEs",
+      url: "https://www.youtube.com/embed/KZzU9YveiiU",
       description:
-        "Squad 37: Episode 3. The squad meets Darveider and the Death Star in a very mortal fight. SPANISH WITH ENGLISH SUBTITLES",
+        "Squad 37: Episode 3. The squad meets Darveider and the Death Star in a very mortal fight. SPANISH",
       type: "YoutubeFrame",
     },
     "e37-2": {
-      url: "https://www.youtube.com/embed/T52i9UZnIZM",
+      url: "https://www.youtube.com/embed/xKSR3wEDAPY",
       description:
-        "Squad 37: Episode 2. Cacho finds a gun and starts shooting everybody. Together with the help of tiny Mario, the team will try to fix it. SPANISH WITH ENGLISH SUBTITLES",
+        "Squad 37: Episode 2. Cacho finds a gun and starts shooting everybody. Together with the help of tiny Mario, the team will try to fix it. SPANISH",
       type: "YoutubeFrame",
     },
     "e37-1": {
-      url: "https://www.youtube.com/embed/oVzXkKnDgCA",
+      url: "https://www.youtube.com/embed/vpME8aREa2c",
       description:
-        "Squad 37: Episode 1. In this first adventure, the squad tries to find Waldo who seems to be hiding somewhere in the spaceship. SPANISH WITH ENGLISH SUBTITLES",
+        "Squad 37: Episode 1. In this first adventure, the squad tries to find Waldo who seems to be hiding somewhere in the spaceship. SPANISH",
       type: "YoutubeFrame",
     },
     social: { type: "SocialMediaFrame" },
