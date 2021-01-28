a266f726097e039be791de7ba7d5f951e3bc6142
Andres Marconato <anmarc@microsoft.com>
2021-01-28 02:42:17 -0300

    fixing toggle on mobile

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,6 +8,10 @@ import cloroImage from "../../images/cloro.png";
 const NavBar = (props) => {
   const [toggle, setToggle] = useState(false);
 
+  const onLinkClick = () => {
+    setToggle(false);
+  };
+
   return (
     <div className="mainWrapper">
       {props.title && <h1 className={toggle ? "hide" : "pageTitle"}>{props.title}</h1>}
@@ -18,13 +22,13 @@ const NavBar = (props) => {
         <div className="wrapper">
           <div className="cloroWrapper"><img className="cloro" src={cloroImage} alt="cloro" /></div>
           <ul className={toggle ? "navLinks showNav" : "navLinks"}>          
-              <Link className="navLink" to="/">Home</Link>
-              <Link className="navLink" to="/digital">Digital</Link>
-              <Link className="navLink" to="/comics">Comics</Link>
-              <Link className="navLink" to="/ink">Ink</Link>
-              <Link className="navLink" to="/escuadron37">Escuadron 37</Link>
+              <Link className="navLink" to="/" onClick={onLinkClick}>Home</Link>
+              <Link className="navLink" to="/digital" onClick={onLinkClick}>Digital</Link>
+              <Link className="navLink" to="/comics" onClick={onLinkClick}>Comics</Link>
+              <Link className="navLink" to="/ink" onClick={onLinkClick}>Ink</Link>
+              <Link className="navLink" to="/escuadron37" onClick={onLinkClick}>Escuadron 37</Link>
               <a className="navLink" href="https://okcerk2.blogspot.com/" target="_blank">Blog</a>
-              <Link className="navLink" to="/about">About</Link>
+              <Link className="navLink" to="/about" onClick={onLinkClick}>About</Link>
               <div style={{ whiteSpace: "nowrap"}}>
                 <SocialMediaLinks />
               </div>
