b9828d2d099389883960580467ea2d4f359b09d3
Andres Marconato <anmarc@microsoft.com>
2021-01-30 23:13:28 -0300

    fixing navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -13,27 +13,25 @@ const NavBar = (props) => {
   };
 
   return (
-    <div className="mainWrapper">
+    <div className="navBar">
       {props.title && <h1 className={toggle ? "hide" : "pageTitle"}>{props.title}</h1>}
-      <div className="navBar">      
-        <button className="navBarButton" onClick={() => setToggle(!toggle)}>
-            <FiAlignJustify />
-        </button>
-        <div className="socialMediaWrapper">
-          <SocialMediaLinks />
-        </div>
-        <div className="wrapper">
-          <div className="cloroWrapper"><img className="cloro" src={cloroImage} alt="cloro" /></div>
-          <ul className={toggle ? "navLinks showNav" : "navLinks"}>          
-              <Link className="navLink" to="/" onClick={onLinkClick}>Home</Link>
-              <Link className="navLink" to="/digital" onClick={onLinkClick}>Digital</Link>
-              <Link className="navLink" to="/comics" onClick={onLinkClick}>Comics</Link>
-              <Link className="navLink" to="/ink" onClick={onLinkClick}>Ink</Link>
-              <Link className="navLink" to="/escuadron37" onClick={onLinkClick}>Escuadron 37</Link>
-              <a className="navLink" href="https://okcerk2.blogspot.com/" target="_blank">Blog</a>
-              <Link className="navLink" to="/about" onClick={onLinkClick}>About</Link>              
-          </ul>          
-        </div>
+      <button className="navBarButton" onClick={() => setToggle(!toggle)}>
+        <FiAlignJustify />
+      </button>
+      <div className="socialMediaWrapper">
+        <SocialMediaLinks />
+      </div>
+      <div className="wrapper">
+        <div className="cloroWrapper"><img className="cloro" src={cloroImage} alt="cloro" /></div>
+        <ul className={toggle ? "navLinks showNav" : "navLinks"}>          
+            <Link className="navLink" to="/" onClick={onLinkClick}>Home</Link>
+            <Link className="navLink" to="/digital" onClick={onLinkClick}>Digital</Link>
+            <Link className="navLink" to="/comics" onClick={onLinkClick}>Comics</Link>
+            <Link className="navLink" to="/ink" onClick={onLinkClick}>Ink</Link>
+            <Link className="navLink" to="/escuadron37" onClick={onLinkClick}>Escuadron 37</Link>
+            <a className="navLink" href="https://okcerk2.blogspot.com/" target="_blank">Blog</a>
+            <Link className="navLink" to="/about" onClick={onLinkClick}>About</Link>              
+        </ul>          
       </div>
     </div>
   );
